311d22f66b302ad39746cc67c08d8b089fc32085
Jacky Ho <jackyhobingo@gmail.com>
2021-08-17 12:17:58 +0800

    Update git alias with show user and email

--- git/git_alias.sh
+++ git/git_alias.sh
@@ -14,4 +14,5 @@ git config --global alias.co checkout
 git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global alias.lg "log --color --graph --pretty=foramt:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -30"
 git config --global alias.l "log --oneline --decorate"
+git config --global alias.sh "!git config user.name & git config user.email"
 
