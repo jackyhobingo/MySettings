757f65f60a950fbb4033be62abd8b6a4ceddc51f
Jacky Ho <jackyhobingo@gmail.com>
2021-08-17 16:00:39 +0800

    Modify git lg adog format

--- git/git_alias.sh
+++ git/git_alias.sh
@@ -11,8 +11,8 @@ git config --global alias.done "!git push origin HEAD"
 git config --global alias.res "reset --hard"
 git config --global alias.cm commit
 git config --global alias.co checkout
-git config --global alias.adog "log --all --decorate --oneline --graph"
-git config --global alias.lg "log --color --graph --pretty=foramt:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -30"
+git config --global alias.lg "log --color --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -30"
+git config --global alias.adog "lg"
 git config --global alias.l "log --oneline --decorate"
 git config --global alias.sh "!git config user.name & git config user.email"
 
